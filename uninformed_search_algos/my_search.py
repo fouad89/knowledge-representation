a1398ded573ec8507537d8407d927e92ef83e904
Fouad <fouad89ahmed@gmail.com>
2021-10-09 08:34:13 +0100

    rewriting search

--- uninformed_search_algos/my_search.py
+++ uninformed_search_algos/my_search.py
@@ -48,3 +48,68 @@ class Problem:
         """Assigns a numeric cost to each path
         """
         return c+1
+
+
+# ----------------------------------------------------------------
+class Node:
+
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+
+        # set depth at 0
+        self.depth = 0
+        # if the node is a parent, add 1 to depth
+        if parent:
+            self.depth = self.depth + 1
+
+
+    def __repr__(self):
+        return "<Node {}>".format(self.state)
+
+    def __lt__(self, node):
+        return self.state < node.state
+
+    def expand(self, problem):
+        """List the nodes reachable in one step from this node
+
+        Args:
+            problem ([type]): [description]
+        """
+        return [self.child_node(problem, action) for action in problem.actions(self.state)]
+
+    def child_node(self, problem, action):
+        next_state = problem.result(self.state, action)
+        next_state = Node(next_state, self, action, problem.path_cost(self.path_cost, self.state, action, next_state))
+        return next_node
+
+    def solution(self):
+        """return the sequence of actions from root to this node
+        """
+        return [node.action for node in self.path()[:1]]
+
+    def path(self):
+        node = self
+        path_back = []
+        # going up the the path
+        while node:
+            path_back.append(node)
+            node = node.parent
+            return list(reversed(path_back))
+
+
+    def __eq__(self, other):
+        return isinstance(other, Node) and self.state == other.state
+
+    def __hash__(self):
+        # We use the hash value of the state
+        # stored in the node instead of the node
+        # object itself to quickly search a node
+        # with the same state in a Hash Table
+        return hash(self.state)
+
+
+# ______________________________________________________________________________
+# Uninformed Search algorithms
